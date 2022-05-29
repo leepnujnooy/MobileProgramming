86daf83aa46f494864c8d4a062b49466b778ce73
leepnujnooy <galaxy160102@gmail.com>
2022-05-30 03:27:17 +0900

    firebase sdk 등록
    등록완료

--- src/main.js
+++ src/main.js
@@ -9,3 +9,21 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBaE-iC480JMPv_r94kqp-rJxJhr0IMak8",
+  authDomain: "mobprac.firebaseapp.com",
+  projectId: "mobprac",
+  storageBucket: "mobprac.appspot.com",
+  messagingSenderId: "592940296101",
+  appId: "1:592940296101:web:da6b90c3ddcc498e6e6a4c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
