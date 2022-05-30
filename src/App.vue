3005a89a5213ebaa11b1731b7ca50214206edc57
leepnujnooy <galaxy160102@gmail.com>
2022-05-31 00:07:23 +0900

    vue router 연결

--- src/App.vue
+++ src/App.vue
@@ -1,115 +1,12 @@
 <template>
-  <v-app>
-    <v-app-bar app color="#6A76AB" dark >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          transition="scale-transition"
-          width="40"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn text>
-        <span></span>
-        <v-icon color="white">mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <TodoHeader />
-      <TodoInput v-on:addTodo="addTodo"></TodoInput>
-      <TodoList
-        v-bind:propsdata="todoItems" 
-        @removeTodo="removeTodo"
-        @editTodo="editTodo"
-        @detailTodo="detailTodo"
-        @toggleComplete="toggleComplete"
-      ></TodoList>
-    </v-main>
-    
-    <v-footer color="#6A76AB" dark>
-      <TodoFooter v-on:removeAll="clearAll" />
-    </v-footer>
-  </v-app>
+  <router-view>
+  </router-view>
 </template>
 
 <script>
-import TodoFooter from "./components/TodoFooter.vue";
-import TodoHeader from "./components/TodoHeader.vue";
-import TodoList from "./components/TodoList.vue";
-import TodoInput from "./components/TodoInput.vue";
-
-export default {
-  name: "App",
-
-  components: {
-    TodoList,
-    TodoFooter,
-    TodoHeader,
-    TodoInput,
-  },
-
-  data() {
-    return {
-      todoItems: []
-    };
-  },
-  methods: {
-    clearAll() {
-      localStorage.clear();
-      this.todoItems = [];
-    },
-    addTodo(todoItem) {
-      //localStorage.setItem(todoItem, JSON.stringify(value));//
-      localStorage.setItem(todoItem,todoItem)
-
-      this.todoItems.push(todoItem);
-    },
-    removeTodo(todoItem, index) {
-      localStorage.removeItem(todoItem);
-      this.todoItems.splice(index, 1);
-    },
-    editTodo(todoItem,index,text) {
-      console.log(todoItem,index,text)
-      this.todoItems.splice(index,1,text)
-      localStorage.setItem(text,text)
-      localStorage.removeItem(todoItem,todoItem)
-    },
-    detailTodo(value,index,todoItem){
-      console.log(value,index,todoItem)
-      this.todoItems.splice(index,1,todoItem)
-      localStorage.setItem(todoItem,value)
-    },
-    toggleComplete(todoItem){
-      todoItem.completed = !todoItem.completed;
-      localStorage.setItem(todoItem.item,JSON.stringify(todoItem))
-    }
-  },
-  created() {
-    if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
-        this.todoItems.push(localStorage.key(i));
-      }
-    }
-  },
-};
 </script>
 
 <style>
-body {
-  text-align: center;
-  background-color: #f6f6f8;
-}
-input {
-  border-style: groove;
-  width: 200px;
-}
-.shadow {
-  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
-}
+
 </style>
 
--- src/main.js
+++ src/main.js
@@ -2,16 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
+import router from './route/index.js'
 
 Vue.config.productionTip = false
 
-new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
-
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,4 +22,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
+
+new Vue({  
+  vuetify,
+  router,
+  render: h => h(App)
+}).$mount('#app')
